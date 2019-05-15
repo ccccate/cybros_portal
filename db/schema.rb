36bcaac70475e58c47c3a9492ec8bbed832d7088
Eric-Guo <eric.guocz@gmail.com>
2019-05-15 09:41:22 +0800

    Will fetch company and update the department when user login.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_13_022901) do
+ActiveRecord::Schema.define(version: 2019_05_15_013415) do
 
   create_table "department_users", force: :cascade do |t|
     t.integer "department_id", null: false
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2019_05_13_022901) do
     t.integer "position"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "company_name"
   end
 
   create_table "name_card_applies", force: :cascade do |t|

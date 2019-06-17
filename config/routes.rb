d6402ac8fffb72fb7a4d3360258b73894efbd537
Eric-Guo <eric.guocz@gmail.com>
2019-06-17 14:33:28 +0800

    structure of complete value reports.

--- config/routes.rb
+++ config/routes.rb
@@ -53,6 +53,7 @@ Rails.application.routes.draw do
     end
     resource :yearly_subsidiary_workloading, only: %i[show]
     resource :contract_signing, only: %i[show]
+    resource :complete_value, only: %i[show]
   end
 
   namespace :person do
